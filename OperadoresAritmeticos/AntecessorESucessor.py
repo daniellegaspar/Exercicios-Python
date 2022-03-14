50d6bdd30792573720b4de923ba07a1f74878018
Danielle Gaspar <danielle.aquinno@hotmail.com>
2022-03-14 17:00:57 -0300

    Create AntecessorESucessor.py

---
+++ OperadoresAritmeticos/AntecessorESucessor.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))
+print('O antecessor do número {} é {}, e o sucessor é {} '.format(n, (n-1), (n+1)))
+
